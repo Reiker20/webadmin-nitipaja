de6a5f8f311e57dac676c9739569af3caac7ff59
? <reikaelvaretta@gmail.com>
2023-08-05 09:50:38 +0700

    Sidebar - reika

---
+++ src/views/Report.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="home-page">
+      <h1>Home Page</h1>
+      <p>Welcome to the Report Page!</p>
+    </main>
+</template>
+  
+<style>
+    /* Styling untuk halaman Home */
+</style>
+  
